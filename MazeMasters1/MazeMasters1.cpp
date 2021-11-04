11ed1fa0722604935d6034a42a3c6ba89469b6a9
Kiril Dimitrov <KGDimitrov19@codingburgas.bg>
2021-11-04 23:13:13 +0200

    Add const func at the begining

--- MazeMasters1/MazeMasters1.cpp
+++ MazeMasters1/MazeMasters1.cpp
@@ -6,6 +6,12 @@ const int HEIGHT = 20;
 unsigned char player = { 1 };
 // Function prototypes
 
+void displayMenu();
+void printMaze(char maze[][WIDTH], int curX, int cury);
+bool validMove(char maze[][WIDTH], int newX, int newY);
+bool move(char maze[][WIDTH], int& curX, int& curY, int newX, int newY);
+// These functions will return true or false if moving to a specified coordinate is valid
+
 void displayMenu()
 {
 	char a;
